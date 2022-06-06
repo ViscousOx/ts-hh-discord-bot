74ab2e5f42fc176049cc11776d081fe28e55a1a5
ViscousOx <jeffreylstamp@gmail.com>
2022-06-06 09:11:49 -0500

    refine message a bit more

--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -22,7 +22,8 @@ export class Example {
       .setColor("#e42643")
       .setTitle("What day should we meet up?")
       .setDescription(
-        "React with the below options to vote for which day works best for you! (voting for multiple days is allowed!) \n\n" +
+        "React with the below options to vote for which day works best for you! (voting for multiple days is allowed!) \n" +
+        "If you would like to add an option that is not here please create a thread with an emoji mapping! \n\n" +
           `${Emojis.hamster} for thursday\n` +
           `${Emojis.bear} for friday`
       );
@@ -55,7 +56,8 @@ export class Example {
       .setColor("#e42643")
       .setTitle("Where should we meet up?")
       .setDescription(
-        "React with the below options to vote for which location you want! (voting for multiple locations is allowed!)\n\n" +
+        "React with the below options to vote for which location you want! (voting for multiple locations is allowed!)\n" +
+        "If you would like to add an option that is not here please create a thread with an emoji mapping! \n\n" +
           `${Emojis.beer} for LynLake Brewery\n` +
           `${Emojis.clink} for Morrissey's Pub\n` +
           `${Emojis.marg} for Lago Tacos\n` +
